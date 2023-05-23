5b31d4ccbc1a298030ab1a940ebd54c17f12be0d
Bogdan Tkachuk <130035186+Butcher13@users.noreply.github.com>
2023-05-23 15:55:37 +0300

    23.05 commit1

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-let element = prompt ("Введіть кількість чисел:");
+let element = prompt ("Введіть кількість елементів в масиві:");
 let array = []
 
 for (let i =0; i<=element; i++) {
-  array[i] =prompt("Введіть число:")
+  array[i] =prompt("Введіть значення елемента:")
 }
 
 for(let i = 0; i <=element; i++) {
   document.write (array[i] + "<br>");
 }
+
+array.sort(function(a, b){return a-b});
+document.write(array + "<br>");
+
+array.splice(2, 3); // Починаючи з індексу 2 видалить  3 елементи
+
+document.write(array);
