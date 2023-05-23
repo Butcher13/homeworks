ba22be7e4773054e9298ea7aaf8715fbd2e13825
Bogdan Tkachuk <130035186+Butcher13@users.noreply.github.com>
2023-05-23 09:24:26 +0300

    first commit

--- main.js
+++ main.js
@@ -1,56 +1,10 @@
+let element = prompt ("Введіть кількість чисел:");
+let array = []
 
-        function submitUserInfo() {
-            const birthYear = prompt('Вкажіть, будь ласка, рік Вашого народження');
-            if (birthYear === null) {
-                alert(`Шкода, що Ви не захотіли ввести свій рік народження`);
-            }
-            let placeOfResidence = prompt('Будь ласка, вкажіть місто, в якому Ви живете');
-            if (placeOfResidence === null) {
-                alert(`Шкода, що Ви не захотіли вказати місто, в якому живете`);
-            } else {
-                placeOfResidence = placeOfResidence.toLowerCase(); //як зробити щоб зберігалась велика буква в назві міста?
-            }
-            let country;
-            switch (placeOfResidence) {
-                case 'київ':
-                    country = 'України';
-                    break;
-                case 'вашингтон':
-                    country = 'США';
-                    break;
-                case 'лондон':
-                    country = 'Великої Британії';
-                    break;
-            }
-            let sport = prompt('Будь ласка, вкажіть Ваш улюблений вид спорту');
-            if (sport === null) {
-                alert(`Шкода, що Ви не захотіли вказати свій улюблений вид спорту`);
-            } else {
-                sport = sport.toLowerCase();
-            }
-            let champion;
-            switch (sport) {
-                case 'футбол':
-                    champion = 'Неманья Відіч';
-                    break;
-                case 'біг':
-                    champion = 'Усейн Болт';
-                    break;
-                case 'бокс':
-                    champion = 'Майк Тайсон';
-                    break;
-            }
-            if (birthYear === null || placeOfResidence === null || sport === null) {
-                alert('Недостатньо даних');
-            } else {
-                if (Boolean(champion) && Boolean(country)) {
-                    alert(`${2023 - birthYear} років. Ви живете у столиці ${country}. Круто! Хочете стати ${champion}?`);
-                } else if (Boolean(country)) {
-                    alert(`${2023 - birthYear} років. Ви живете у столиці ${country}.`);
-                } else if (Boolean(champion)) {
-                    alert(`${2023 - birthYear} років. Ви живете у місті ${placeOfResidence}. Круто! Хочете стати ${champion}?`);
-                } else {
-                    alert(`${2023 - birthYear} років. Ви живете у місті ${placeOfResidence}. Ваш улюблений вид спорту ${sport}`);
-                }
-            }
-        }
+for (let i =0; i<=element; i++) {
+  array[i] =prompt("Введіть число:")
+}
+
+for(let i = 0; i <=element; i++) {
+  document.write (array[i] + "<br>");
+}
